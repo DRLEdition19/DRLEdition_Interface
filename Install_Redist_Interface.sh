2ac6d3451cb68d28a57dc1e63321d8286c2cae33
DRLEdition19 <drledition19@gmail.com>
2025-03-25 19:39:35 -0300

    Update Install_Redist_Interface.sh

--- Install_Redist_Interface.sh
+++ Install_Redist_Interface.sh
@@ -110,7 +110,7 @@ mkdir -p $PORTS_DIR
 
 # Download the DRL file
 echo "Downloading the Redist_Files.DRL file..."
-curl -L -o $DRL_FILE "https://github.com/DRLEdition19/Redist_Interface/releases/download/files/Redist_Files.DRL"
+curl -L -o $DRL_FILE "https://github.com/DRLEdition19/DRLEdition_Interface/releases/download/files/Redist_Files.DRL"
 
 # Check if download was successful
 if [ ! -f "$DRL_FILE" ]; then
