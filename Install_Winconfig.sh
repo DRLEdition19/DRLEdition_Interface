acadf10afcde839878764ef3e71b3cabcbc8f8b8
DRLEdition19 <drledition19@gmail.com>
2025-06-06 10:39:18 -0300

    Create Install_Winconfig.sh

---
+++ Install_Winconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Exibe mensagem inicial 
+echo "The 'WinConfig - Windows Game Fix' tool is undergoing maintenance."
+echo "It will soon be ready to be installed and used."
+echo "Thank you for your understanding."
+echo "ThIt will be available within 24 hours, see you soon!"
+exit 0
