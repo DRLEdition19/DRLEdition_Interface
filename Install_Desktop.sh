792b11e67c547105bcd1aa1267c9b4d2f7bfaf76
DRLEdition19 <drledition19@gmail.com>
2025-06-23 17:57:51 -0300

    Update Install_Desktop.sh

--- Install_Desktop.sh
+++ Install_Desktop.sh
@@ -90,7 +90,7 @@ clear
 
 
 # Welcome message
-echo "Welcome to the automatic installer for the Desktop_for_Batocera 8.0 by DRL Edition."
+echo "Welcome to the automatic installer for the Desktop_for_Batocera 8.1 by DRL Edition."
 
 # Temporary directory for download
 TEMP_DIR="/userdata/tmp/Desktop_for_Batocera"
@@ -147,6 +147,6 @@ batocera-save-overlay
 clear
 echo "Installation completed successfully."
 echo "For the Desktop to work properly, you will need to restart your machine."
-echo "Desktop_for_Batocera 8.0 by DRL Edition"
+echo "Desktop_for_Batocera 8.1 by DRL Edition"
 
 exit 0
