a7f0cc3c2816ed0aa69c53759460fb87a05155f5
DRLEdition19 <drledition19@gmail.com>
2025-06-28 17:54:15 -0300

    Update Install_Android.sh

--- Android/Install_Android.sh
+++ Android/Install_Android.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Download do arquivo
 echo "Baixando o arquivo..."
-wget -O /tmp/Install_Android_Oficial.sh https://github.com/DRLEdition19/DRLEdition_Interface/raw/refs/heads/main/Android/Install_Android_Oficial.sh
+wget -O /userdata/tmp/Install_Android_Oficial.sh https://github.com/DRLEdition19/DRLEdition_Interface/raw/refs/heads/main/Android/Install_Android_Oficial.sh
 echo "Download concluído com sucesso."
 
 # Configura as permissões
 echo "Configurando permissões (chmod 777)..."
-chmod 777 /tmp/Install_Android_Oficial.sh
+chmod 777 /userdata/tmp/Install_Android_Oficial.sh
 clear
 
 # Inicia a ferramenta para configurar o Idioma
 xterm -fs 14 -fg white -bg black -fa "Monospace" -en UTF-8 -sb -rightbar -e bash -c "PS1='[\u@\h \$PWD]# ' /bin/bash /tmp/Install_Android_Oficial.sh"
 
-rm -r -f /tmp/Install_Android_Oficial.sh
+rm -r -f /userdata//tmp/Install_Android_Oficial.sh
