a2ba21de5162884ac579e1d4155e28581fe705be
DRLEdition19 <drledition19@gmail.com>
2025-07-20 15:19:30 -0300

    Update Install_Desktop.sh

--- Install_Desktop.sh
+++ Install_Desktop.sh
@@ -110,7 +110,7 @@ clear
 
 # Download the DRL file
 echo "Downloading the DRL file..."
-curl -L -o $DRL_FILE "https://github.com/DRLEdition19/DRLEdition_Interface/releases/download/files/Desktop_for_batocera_8.6.DRL"
+curl -L -o $DRL_FILE "https://github.com/DRLEdition19/DRLEdition_Interface/releases/download/files/Desktop_for_batocera_8.6.1.DRL"
 
 # Check if download was successful
 if [ ! -f "$DRL_FILE" ]; then
